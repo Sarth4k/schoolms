78a8692daa90d3d5cf039c0567535db8478d8af0
Sarth4k <dishusaini1979@gmail.com>
2026-07-06 15:28:39 +0530

    uhh

--- api/urls.py
+++ api/urls.py
@@ -8,12 +8,15 @@ TokenRefreshView,
 )
 
 urlpatterns = [
-    path('', views.subjects_list),
+    path('subjects/', views.subjects_list),
     path('token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
     path('token/refresh/',TokenRefreshView.as_view()),
-    path('<int:pk>/',views.subjects_detail),
-    #path('<int:pk>/update/',views.subjects_update),
-    path('<int:pk>/delete/',views.subjects_delete),
+    path('subjects/<int:pk>/',views.subjects_detail),
+    path('subjects/<int:pk>/delete/',views.subjects_delete),
     path('login/',views.login_view),
     path('register/',views.register_view),
+    path('student/',views.student_profile),
+    path('student/update/',views.student_profile_update),
+    path('student/admissionstatus/',views.student_admission_status),
+    path('student/enrolled/',views.student_enrolled_subjects),
 ]
